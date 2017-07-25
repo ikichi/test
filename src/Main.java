3a8d38e255e82af1923a5607064f3ae808db5d3e
Petrov Vasiliy <vpetrov@odin.com>
2017-07-25 10:26:53 +0700

    Many messages

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World from branch!");
+        System.out.println("Hello World from branch!");
+        System.out.println("Hello World from branch!");
+        System.out.println("Hello World from branch!");
     }
 }
